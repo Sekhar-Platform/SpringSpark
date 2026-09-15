debd64e1839fc56b580222d97fdb07609e0785b0
Sekhar Panda <pandasdsekhar@gmail.com>
2026-09-15 16:03:39 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/example/sachin/SachinCelebrationApplication.java
@@ -0,0 +1,12 @@
+package com.example.sachin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachinCelebrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SachinCelebrationApplication.class, args);
+    }
+}
